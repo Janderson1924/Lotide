edb87d8060e457e4a181a5e8aab99e37396a2c3e
Justin <justinanderson101@gmail.com>
2021-03-08 22:36:57 +0000

    refactored with mocha/chai

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -4,3 +4,12 @@ const assertEqual = require('../assertEqual');
 // assertEqual tests
 // assertEqual("Lighthouse Labs", "Bootcamp");
 // assertEqual(1, 1);
+
+describe("should tell us if two items match or not", () => {
+  it("returns yes if matching", () => {
+    assert.strictEqual(assertEqual(19, 19));
+  });
+  it("returns no if different", () => {
+    assert.strictEqual(assertEqual(4, 5)); 
+  });
+});
