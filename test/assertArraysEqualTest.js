5b01b7abb395c149b3984662e0edea182554df95
Justin <justinanderson101@gmail.com>
2021-03-08 22:41:53 +0000

    refactored with mocha/chai

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,6 +1,12 @@
-const eqArrays = require('../eqArrays.js');
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual.js')
 
-//tests
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 5]);
+
+describe("should tell us if two arrays are equal", () => {
+  it("returns yes if matching", () => {
+    assert.deepEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+  });
+  it("returns no if different", () => {
+    assert.deepEqual(assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 5])); 
+  });
+});
