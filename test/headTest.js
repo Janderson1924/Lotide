1d6b3a22924494e98dc4a349a081b3899a4e9f31
Justin <justinanderson101@gmail.com>
2021-03-08 21:50:48 +0000

    refactored with mocha/chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,10 +1,3 @@
-// const assertEqual = require('../assertEqual.js')
-// const head = require('../head.js')
-
-//tests
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
 const assert = require('chai').assert;
 const head = require('../head.js');
 
@@ -12,10 +5,7 @@ describe("should return first item in an array", () => {
   it("returns 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
-});
-
-describe("should return first item in an array", () => {
   it("returns '5' for ['5']", () => {
-    assert.strictEqual(head(['6']), '6');
+    assert.strictEqual(head(['5']), '5'); 
   });
 });
