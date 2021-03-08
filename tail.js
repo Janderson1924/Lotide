b0cc97ad5baf0f72c76246713b36ce48b7d6faee
Justin <justinanderson101@gmail.com>
2021-03-08 20:48:31 +0000

    refactored with require/export

--- tail.js
+++ tail.js
@@ -1,19 +1,7 @@
-const assertEqual = function(actual,expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-
-const words = ["Yo Yo", "Lighthouse", "Labs", "Dawg"];
-
 const tail = function(array) {
   return array.slice(1, 4);
 };
 
-console.log(tail(words));
+module.exports = tail;
+
+
