338813fb9d19cf272a58d1fcf9af3e4bcd84cfe4
Justin <justinanderson101@gmail.com>
2021-03-08 22:25:25 +0000

    refactored with mocha/chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,15 @@
+const assert = require('chai').assert;
 const tail = require('../tail.js')
 
 // tests
-const words = ["Yo Yo", "Lighthouse", "Labs", "Dawg"];
-console.log(tail(words));
+// const words = ["Yo Yo", "Lighthouse", "Labs", "Dawg"];
+// console.log(tail(words));
+
+describe("should show 'tail' of given array (all but the first item)", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns [4, 8, 3] for [1, 4, 8, 3]", () => {
+    assert.deepEqual(tail([1, 4, 8, 3]), [4, 8, 3]); 
+  });
+});
